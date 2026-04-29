9cd058973f0b24b9c60d6f6e73b8bf1cc13e2d6a
Eddy HUART <e.ehuart@etu.cvm.qc.ca>
2026-04-29 16:29:10 -0400

    Fix player

--- EddysGym/DpmCharacterController.cs
+++ EddysGym/DpmCharacterController.cs
@@ -4,30 +4,41 @@ using Utils;
 public partial class DpmCharacterController : Node2D
 {
     [ExportGroup("Reference")]
-    [Export] private CharacterBody2D _body;
+    [Export]
+    private CharacterBody2D _body;
 
-    [Export] private AnimatedSprite2D _sprite;
+    [Export]
+    private AnimatedSprite2D _sprite;
 
     [ExportGroup("Movement")]
-    [Export] public float MoveSpeed = 400.0f;
+    [Export]
+    public float MoveSpeed = 400.0f;
 
-    [Export] public float MoveAcceleration = 4000.0f;
+    [Export]
+    public float MoveAcceleration = 4000.0f;
 
     [ExportGroup("Jump")]
-    [Export] public float CoyoteJumpBoost = 1.0f;
+    [Export]
+    public float CoyoteJumpBoost = 1.0f;
 
-    [Export] public float JumpInitialVelocity = -200.0f;
+    [Export]
+    public float JumpInitialVelocity = -200.0f;
 
-    [Export] public float JumpMaxVelocity = -600.0f;
+    [Export]
+    public float JumpMaxVelocity = -600.0f;
 
-    [Export] public float JumpSustainForce = -20.0f;
+    [Export]
+    public float JumpSustainForce = -20.0f;
 
-    [Export] public float JumpSustainMaxTime = 0.50f;
+    [Export]
+    public float JumpSustainMaxTime = 0.50f;
 
-    [Export] public float CoyoteDuration = 0.15f;
+    [Export]
+    public float CoyoteDuration = 0.15f;
 
     [ExportGroup("Physics")]
-    [Export] public float FallSpeedCap = 1000.0f;
+    [Export]
+    public float FallSpeedCap = 1000.0f;
 
     private float _moveAxis = 0.0f;
     private float _facingDir = 1.0f;
@@ -188,37 +199,47 @@ public partial class DpmCharacterController : Node2D
             return;
 
         if (_sprite.Animation != anim)
-            _sprite.Play(anim);
+        {
+            _sprite.Stop();
+            _sprite.Animation = anim;
+            _sprite.Frame = 0;
+            _sprite.FrameProgress = 0.0f;
+            _sprite.Play();
+        }
     }
 
-private string DetermineAnim()
-{
-    if (_body.IsOnFloor())
+    private string DetermineAnim()
     {
-        _coyoteJumpTriggered = false;
+        if (_body.IsOnFloor())
+        {
+            _coyoteJumpTriggered = false;
 
-        if (Mathf.Abs(_body.Velocity.X) > 5.0f)
-            return "walk";
+            if (Mathf.Abs(_body.Velocity.X) > 5.0f)
+                return "walk";
+
+            return "idle";
+        }
+
+        if (_body.Velocity.Y < 0.0f)
+        {
+            if (
+                _coyoteJumpTriggered
+                && _sprite.SpriteFrames != null
+                && _sprite.SpriteFrames.HasAnimation("coyote")
+            )
+                return "coyote";
+
+            return "jump";
+        }
+
+        if (_body.Velocity.Y >= 0.0f)
+        {
+            if (_sprite.SpriteFrames != null && _sprite.SpriteFrames.HasAnimation("fall"))
+                return "fall";
+
+            return "jump";
+        }
 
         return "idle";
     }
-
-    if (_body.Velocity.Y < 0.0f)
-    {
-        if (_coyoteJumpTriggered && _sprite.SpriteFrames != null && _sprite.SpriteFrames.HasAnimation("coyote"))
-            return "coyote";
-
-        return "jump";
-    }
-
-    if (_body.Velocity.Y >= 0.0f)
-    {
-        if (_sprite.SpriteFrames != null && _sprite.SpriteFrames.HasAnimation("fall"))
-            return "fall";
-
-        return "jump";
-    }
-
-    return "idle";
-}
 }
