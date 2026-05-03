e2040908abd64dc62a0c9613a24a7026252a51aa
Charlee <cha.cyr.d@gmail.com>
2026-05-03 19:42:32 -0400

    ajout sans_cervelle, goomba 2.0

---
+++ Gyms/CharleeGym/scripts/Enemy.cs
@@ -0,0 +1,72 @@
+using Godot;
+
+public partial class Enemy : CharacterBody2D
+{
+    [ExportGroup("External")]
+    [Export]
+    public AnimatedSprite2D Sprite;
+
+    [Export]
+    private int Vies;
+
+    public void Take_Damage(Node2D body)
+    {
+        if (body is Projectile)
+        {
+            Vies -= 1;
+            Flash_Red();
+
+            if (Vies <= 0)
+            {
+                Be_Destroyed();
+            }
+        }
+    }
+
+    public void Flash_Red()
+    {
+        if (Sprite.Material is not ShaderMaterial)
+            return;
+
+        var mat = (ShaderMaterial)Sprite.Material;
+
+        var tween = CreateTween();
+
+        tween.TweenMethod(
+            Callable.From<float>(value => mat.SetShaderParameter("flash_strength", value)),
+            0.0f,
+            1.0f,
+            0.05f
+        );
+        tween.TweenMethod(
+            Callable.From<float>(value => mat.SetShaderParameter("flash_strength", value)),
+            1.0f,
+            0.0f,
+            0.2f
+        );
+    }
+
+    public void Be_Destroyed()
+    {
+        if (Sprite.Material is not ShaderMaterial)
+        {
+            QueueFree();
+            return;
+        }
+
+        var mat = (ShaderMaterial)Sprite.Material;
+
+        var tween = CreateTween();
+
+        tween.TweenMethod(
+            Callable.From<float>(value => mat.SetShaderParameter("death_strength", value)),
+            0.0f,
+            1.0f,
+            0.1f
+        );
+
+        tween.TweenInterval(0.3f);
+
+        tween.Finished += QueueFree;
+    }
+}
---
+++ Gyms/CharleeGym/scripts/Helpers/WalkTowardsPlayer.cs
@@ -0,0 +1,35 @@
+using Godot;
+
+public partial class WalkTowardsPlayer : Node2D
+{
+	[ExportGroup("Walk")]
+	[Export]
+	public bool Is_Active = false;
+
+	[Export]
+	public CharacterBody2D OwnerNode;
+
+	private int _Direction_Faced = 1;
+	private float _Gravity_Force = (float)ProjectSettings.GetSetting("physics/2d/default_gravity");
+
+	public void Walk_Towards_Player(CharacterBody2D Character, AnimatedSprite2D Sprite, float Speed)
+	{
+		if (Is_Active)
+		{
+			float _DirectionX = Mathf.Sign(Character.GlobalPosition.X - OwnerNode.GlobalPosition.X);
+			int _Direction_Temp = _DirectionX < 0 ? 1 : -1;
+			if (_Direction_Temp != _Direction_Faced)
+			{
+				Sprite.FlipH = _Direction_Temp < 0;
+				_Direction_Faced = _Direction_Temp;
+			}
+
+			OwnerNode.Velocity = new Vector2(
+				_DirectionX * Speed,
+				OwnerNode.Velocity.Y + _Gravity_Force
+			);
+			OwnerNode.MoveAndSlide();
+			OwnerNode.GlobalPosition = OwnerNode.GlobalPosition.Round();
+		}
+	}
+}
