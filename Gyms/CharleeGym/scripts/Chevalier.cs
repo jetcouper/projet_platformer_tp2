fd9a56243ba4973f21ed47e205b669a079bfbdfb
Charlee <cha.cyr.d@gmail.com>
2026-05-03 22:42:44 -0400

    Commence chevalier

---
+++ Gyms/CharleeGym/scripts/Chevalier.cs
@@ -0,0 +1,71 @@
+using System;
+using System.Diagnostics;
+using Godot;
+
+public partial class Chevalier : Enemy
+{
+	[ExportGroup("Attack")]
+	[Export]
+	public float MaxWait;
+
+	[Export]
+	public Timer Timer;
+
+	[Export]
+	public CharacterBody2D Character;
+	private float NextShot;
+	private int _Direction_Faced;
+
+	private float _Gravity_Force = (float)ProjectSettings.GetSetting("physics/2d/default_gravity");
+
+	public override void _Ready()
+	{
+		Timer.Timeout += Attack;
+		RestartTimer();
+	}
+
+	public override void _PhysicsProcess(double delta)
+	{
+		float _DirectionX = Mathf.Sign(Character.GlobalPosition.X - GlobalPosition.X);
+		int _Direction_Temp = _DirectionX < 0 ? 1 : -1;
+		if (_Direction_Temp != _Direction_Faced)
+		{
+			Sprite.FlipH = _Direction_Temp < 0;
+			_Direction_Faced = _Direction_Temp;
+		}
+
+		Velocity = new Vector2(Velocity.X, Velocity.Y + _Gravity_Force);
+
+		MoveAndSlide();
+	}
+
+	public void RandomNextShot()
+	{
+		Random rand = new Random();
+
+		NextShot = (float)(rand.NextDouble() * MaxWait);
+	}
+
+	public void RestartTimer()
+	{
+		Sprite.Play("idle");
+		RandomNextShot();
+		Timer.WaitTime = NextShot;
+		Timer.Start();
+	}
+
+	public async void Attack()
+	{
+		Sprite.Play("attack");
+
+		await ToSignal(GetTree().CreateTimer(0.5), "timeout");
+
+		Shoot();
+
+		await ToSignal(GetTree().CreateTimer(0.5), "timeout");
+
+		RestartTimer();
+	}
+
+	public void Shoot() { }
+}
