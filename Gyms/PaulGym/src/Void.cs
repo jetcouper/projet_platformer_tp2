d595439569597cf114429c9852e0ba380abdd5e9
Paul-EdouardFort <fortinpauledouard@gmail.com>
2026-05-05 21:18:50 -0400

    voids

---
+++ Gyms/PaulGym/src/Void.cs
@@ -0,0 +1,25 @@
+using Godot;
+using System;
+
+public partial class Void : Area2D
+{
+	[Export]
+	CharacterBody2D player;
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+	}
+	public void _on_body_entered(Node2D areaContact)
+	{
+		if(areaContact == player)
+		{
+			DpmHealth health = areaContact.GetNodeOrNull<DpmHealth>("DpmHealth");
+        	health?.TakeDamage((int)99999999);
+		}
+	}
+}
