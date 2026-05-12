04f06226f67b1135080b2ccf11fb0fe5205b44f9
jetcouper <ant7945@gmail.com>
2026-05-12 15:13:11 -0400

    UPDATE: Début de niveau2.tscn

--- Library/DCM/DcmWindSystem.cs
+++ Library/DCM/DcmWindSystem.cs
@@ -3,47 +3,51 @@ using Godot;
 
 public partial class DcmWindSystem : Node2D
 {
-	private float _SpeedMove = 1.0f;
+    private float _SpeedMove = 1.0f;
 
-	[Export]
-	public float SpeedMove
-	{
-		get => _SpeedMove;
-		set
-		{
-			//Style early return pour éviter les if imbriqués
-			if (Mathf.IsEqualApprox(_SpeedMove, value))
-			{
-				return;
-			}
-			//Value = valeur passée au setter
-			_SpeedMove = value;
-			SetSpeedMove(_SpeedMove);
-		}
-	}
+    [Export]
+    public float SpeedMove
+    {
+        get => _SpeedMove;
+        set
+        {
+            //Style early return pour éviter les if imbriqués
+            if (Mathf.IsEqualApprox(_SpeedMove, value))
+            {
+                return;
+            }
+            //Value = valeur passée au setter
+            _SpeedMove = value;
+            SetSpeedMove(_SpeedMove);
+        }
+    }
 
-	// Called when the node enters the scene tree for the first time.
-	public override void _Ready()
-	{
-		Tween tw = CreateTween();
-		tw.TweenProperty(this, "SpeedMove", _SpeedMove + 0.5f, 2.0f);
-		tw.TweenProperty(this, "SpeedMove", _SpeedMove - 0.5f, 2.0f);
-		tw.SetLoops();
-	}
+    // Called when the node enters the scene tree for the first time.
+    public override void _Ready()
+    {
+        Tween tw = CreateTween();
+        tw.TweenProperty(this, "SpeedMove", _SpeedMove + 0.5f, 2.0f);
+        tw.TweenProperty(this, "SpeedMove", _SpeedMove - 0.5f, 2.0f);
+        tw.SetLoops();
+    }
 
-	public void SetSpeedMove(float InSpeedMove)
-	{
-		//GetChildren() retourne les enfants directes(Pas les petits-enfants)
-		foreach (Node2D child in GetChildren())
-		{
-			if (child is Oscillation osc)
-			{
-				osc.SetSpeedMove(InSpeedMove);
-			}
-			else if (child is Oscillation osc2)
-			{
-				osc2.SetSpeedMove(InSpeedMove);
-			}
-		}
-	}
+    public void SetSpeedMove(float InSpeedMove)
+    {
+        //GetChildren() retourne les enfants directes(Pas les petits-enfants)
+        foreach (Node2D child in GetChildren())
+        {
+            if (child is Oscillation osc)
+            {
+                osc.SetSpeedMove(InSpeedMove);
+            }
+            else if (child is OscillationOiseau osc2)
+            {
+                osc2.SetSpeedMove(InSpeedMove);
+            }
+            else if (child is OscillationBat osc3)
+            {
+                osc3.SetSpeedMove(InSpeedMove);
+            }
+        }
+    }
 }
---
+++ Library/Utils/OscillationBat.cs
@@ -0,0 +1,51 @@
+using System;
+using Godot;
+
+public partial class OscillationBat : Node2D
+{
+    [Export]
+    private AnimatedSprite2D node;
+
+    [Export]
+    private Vector2 limitRange = new(-700, 700);
+
+    [Export]
+    private Vector2 durationRange = new(10.0f, 40.0f);
+
+    private Tween _Tween;
+
+    public override void _Ready()
+    {
+        if (!IsInstanceValid(node))
+        {
+            return;
+        }
+
+        float randomDuration = (float)GD.RandRange(durationRange.X, durationRange.Y);
+        float initialX = node.Position.X;
+
+        _Tween = CreateTween();
+        //Tweener supportent le chaînage (appels subséquants de méthodes)
+        node.Play();
+        _Tween.TweenCallback(Callable.From(() => node.Play("moveLeft")));
+        _Tween.TweenCallback(Callable.From(() => node.FlipH = false));
+        _Tween
+            .TweenProperty(node, "position:x", initialX + limitRange.X, randomDuration)
+            .SetEase(Tween.EaseType.InOut)
+            .SetTrans(Tween.TransitionType.Linear);
+
+        _Tween.TweenCallback(Callable.From(() => node.Play("moveRight")));
+        _Tween.TweenCallback(Callable.From(() => node.FlipH = true));
+        _Tween
+            .TweenProperty(node, "position:x", initialX + limitRange.Y, randomDuration)
+            .SetEase(Tween.EaseType.InOut)
+            .SetTrans(Tween.TransitionType.Linear);
+
+        _Tween.SetLoops();
+    }
+
+    public void SetSpeedMove(float InSpeedScale)
+    {
+        _Tween?.SetSpeedScale(InSpeedScale);
+    }
+}
