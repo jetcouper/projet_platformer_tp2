490252e84bafec049164c38acdc1c6baa0764f31
Eddy HUART <e.ehuart@etu.cvm.qc.ca>
2026-05-13 11:38:09 -0400

    Ajout nuages sombres avec particules de pluie

--- Library/Utils/Oscillation.cs
+++ Library/Utils/Oscillation.cs
@@ -3,43 +3,43 @@ using Godot;
 
 public partial class Oscillation : Node2D
 {
-	[Export]
-	private Node2D node;
+    [Export]
+    private Node2D node;
 
-	[Export]
-	private Vector2 limitRange = new(-700, 700);
+    [Export]
+    private Vector2 limitRange = new(-700, 700);
 
-	[Export]
-	private Vector2 durationRange = new(10.0f, 40.0f);
+    [Export]
+    private Vector2 durationRange = new(10.0f, 40.0f);
 
-	private Tween _Tween;
+    private Tween _Tween;
 
-	public override void _Ready()
-	{
-		if (!IsInstanceValid(node))
-		{
-			return;
-		}
+    public override void _Ready()
+    {
+        if (!IsInstanceValid(node))
+        {
+            return;
+        }
 
-		float randomDuration = (float)GD.RandRange(durationRange.X, durationRange.Y);
-		float initialX = node.Position.X;
+        float randomDuration = (float)GD.RandRange(durationRange.X, durationRange.Y);
+        float initialX = node.Position.X;
 
-		_Tween = CreateTween();
-		//Tweener supportent le chaînage (appels subséquants de méthodes)
-		_Tween
-			.TweenProperty(node, "position:x", initialX + limitRange.X, randomDuration)
-			.SetEase(Tween.EaseType.InOut)
-			.SetTrans(Tween.TransitionType.Sine);
-		_Tween
-			.TweenProperty(node, "position:x", initialX + limitRange.Y, randomDuration)
-			.SetEase(Tween.EaseType.InOut)
-			.SetTrans(Tween.TransitionType.Sine);
+        _Tween = CreateTween();
+        //Tweener supportent le chaînage (appels subséquants de méthodes)
+        _Tween
+            .TweenProperty(node, "position:x", initialX + limitRange.X, randomDuration)
+            .SetEase(Tween.EaseType.InOut)
+            .SetTrans(Tween.TransitionType.Sine);
+        _Tween
+            .TweenProperty(node, "position:x", initialX + limitRange.Y, randomDuration)
+            .SetEase(Tween.EaseType.InOut)
+            .SetTrans(Tween.TransitionType.Sine);
 
-		_Tween.SetLoops();
-	}
+        _Tween.SetLoops();
+    }
 
-	public void SetSpeedMove(float InSpeedScale)
-	{
-		_Tween?.SetSpeedScale(InSpeedScale);
-	}
+    public void SetSpeedMove(float InSpeedScale)
+    {
+        _Tween?.SetSpeedScale(InSpeedScale);
+    }
 }
