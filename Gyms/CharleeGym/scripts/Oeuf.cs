06361a92a77fa998cf23d214c96c72b93bb56528
Charlee <cha.cyr.d@gmail.com>
2026-05-15 15:04:48 -0400

    bug oeuf

--- Gyms/CharleeGym/scripts/Oeuf.cs
+++ Gyms/CharleeGym/scripts/Oeuf.cs
@@ -3,39 +3,58 @@ using Godot;
 
 public partial class Oeuf : Enemy
 {
-	[Export]
-	public Shield Shield;
+    [Export]
+    public Shield Shield;
 
-	[Export]
-	public bool IngredientState = true;
+    [Export]
+    public bool IngredientState = true;
 
-	[Export]
-	public CharacterBody2D Character;
+    [Export]
+    public CharacterBody2D Character;
 
-	public override void _Ready()
-	{
-		Sprite.Play("inactive");
-		ApplyIngredientState();
-	}
+    private int _Direction_Faced;
 
-	public void SetIngredientState(bool state)
-	{
-		IngredientState = state;
-		ApplyIngredientState();
-	}
+    private float _Gravity_Force = (float)ProjectSettings.GetSetting("physics/2d/default_gravity");
 
-	public new void Take_Damage(Node2D body)
-	{
-		if (Shield?.IsActive == true)
-		{
-			return;
-		}
+    public override void _Ready()
+    {
+        Sprite.Play("inactive");
+        ApplyIngredientState();
+    }
 
-		base.Take_Damage(body);
-	}
+    public override void _PhysicsProcess(double delta)
+    {
+        float _DirectionX = Mathf.Sign(Character.GlobalPosition.X - GlobalPosition.X);
+        int _Direction_Temp = _DirectionX < 0 ? 1 : -1;
+        if (_Direction_Temp != _Direction_Faced)
+        {
+            Sprite.FlipH = _Direction_Temp < 0;
+            _Direction_Faced = _Direction_Temp;
+        }
 
-	private void ApplyIngredientState()
-	{
-		Shield?.SetActive(IngredientState);
-	}
+        Velocity = new Vector2(0, Velocity.Y + _Gravity_Force);
+
+        MoveAndSlide();
+    }
+
+    public void SetIngredientState(bool state)
+    {
+        IngredientState = state;
+        ApplyIngredientState();
+    }
+
+    public new void Take_Damage(Node2D body)
+    {
+        if (Shield?.IsActive == true)
+        {
+            return;
+        }
+
+        base.Take_Damage(body);
+    }
+
+    private void ApplyIngredientState()
+    {
+        Shield?.SetActive(IngredientState);
+    }
 }
